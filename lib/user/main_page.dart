dacb3287fd1e1c18a40efb7a0cd3c737ecd2d3c5
kypdev <kyptdev@gmail.com>
2020-04-08 14:09:37 +0700

    fix appbar main page user

--- lib/user/main_page.dart
+++ lib/user/main_page.dart
@@ -13,242 +13,210 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+  profile(){
+    Navigator.of(context).push(_createRoute(screen: ProfileUser()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.topCenter,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  appBars(
-                    h: MediaQuery.of(context).size.height / 7,
-                    c: context,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 16, right: 16, bottom: 10, top: 12),
-                    child: Text(
-                      'โปรโมชั่นแนะนำ',
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontFamily: mali,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 10,
-                    ),
-                    height: MediaQuery.of(context).size.height * 0.40,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                        promotion(
-                          img: 'assets/images/12.jpg',
-                        ),
-                        promotion(
-                          img: 'assets/images/11.jpg',
-                        ),
-                        promotion(
-                          img: 'assets/images/13.jpg',
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 16, right: 16, bottom: 10, top: 12),
-                    child: Text(
-                      'มาใหม่ !',
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        fontFamily: mali,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 10.0,
-                    ),
-                    height: MediaQuery.of(context).size.height * 0.38,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                        add(
-                          img: 'assets/images/05.jpg',
-                        ),
-                        add(
-                          img: 'assets/images/02.jpg',
-                        ),
-                        add(
-                          img: 'assets/images/03.jpg',
-                        ),
-                        add(
-                          img: 'assets/images/04.jpg',
-                        ),
-                        add(
-                          img: 'assets/images/01.jpg',
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 16, right: 16, bottom: 10, top: 12),
-                    child: Text(
-                      'ร้านแนะนำ',
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        fontFamily: mali,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 16.0,
-                    ),
-                    height: 120,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                        shop(
-                          img: 'assets/images/33.png',
-                        ),
-                        shop(
-                          img: 'assets/images/32.jpg',
-                        ),
-                        shop(
-                          img: 'assets/images/34.jpg',
-                        ),
-                        shop(
-                          img: 'assets/images/logocof.jpg',
-                        ),
-                        shop(
-                          img: 'assets/images/31.jpg',
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 16, right: 16, bottom: 10, top: 12),
-                    child: Text(
-                      'ร้าน',
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        fontFamily: mali,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 16.0,
-                    ),
-                    height: MediaQuery.of(context).size.height ,
-                    child: ListView(
-                      scrollDirection: Axis.vertical,
-                      children: <Widget>[
-                        store(
-                          img: 'assets/images/41.jpg',
-                          nameStore: 'Fire tiger',
-                        ),
-                        store(
-                          img: 'assets/images/42.jpg',
-                          nameStore: 'ชานม',
-                        ),
-                        store(
-                          img: 'assets/images/43.jpg',
-                          nameStore: 'ซูชิ',
-                        ),
-                        store(
-                          img: 'assets/images/44.jpg',
-                          nameStore: 'Odtomato',
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 100.0,
-                  ),
-                ],
+      appBar: AppBar(
+        title: Row(
+          children: <Widget>[
+            Expanded(child: Text('หน้าแรก', style: TextStyle(fontFamily: mali), textAlign: TextAlign.center,)),
+            GestureDetector(
+              onTap: profile,
+              child: Icon(
+                Icons.account_circle,
+                size: 45,
+                color: Colors.white,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.topCenter,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, bottom: 10, top: 12),
+                      child: Text(
+                        'โปรโมชั่นแนะนำ',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontFamily: mali,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                      ),
+                      height: MediaQuery.of(context).size.height * 0.40,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          promotion(
+                            img: 'assets/images/12.jpg',
+                          ),
+                          promotion(
+                            img: 'assets/images/11.jpg',
+                          ),
+                          promotion(
+                            img: 'assets/images/13.jpg',
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, bottom: 10, top: 12),
+                      child: Text(
+                        'มาใหม่ !',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          fontFamily: mali,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                      height: MediaQuery.of(context).size.height * 0.38,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          add(
+                            img: 'assets/images/05.jpg',
+                          ),
+                          add(
+                            img: 'assets/images/02.jpg',
+                          ),
+                          add(
+                            img: 'assets/images/03.jpg',
+                          ),
+                          add(
+                            img: 'assets/images/04.jpg',
+                          ),
+                          add(
+                            img: 'assets/images/01.jpg',
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, bottom: 10, top: 12),
+                      child: Text(
+                        'ร้านแนะนำ',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          fontFamily: mali,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                      ),
+                      height: 120,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          shop(
+                            img: 'assets/images/33.png',
+                          ),
+                          shop(
+                            img: 'assets/images/32.jpg',
+                          ),
+                          shop(
+                            img: 'assets/images/34.jpg',
+                          ),
+                          shop(
+                            img: 'assets/images/logocof.jpg',
+                          ),
+                          shop(
+                            img: 'assets/images/31.jpg',
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, bottom: 10, top: 12),
+                      child: Text(
+                        'ร้าน',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          fontFamily: mali,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                      ),
+                      height: MediaQuery.of(context).size.height ,
+                      child: ListView(
+                        scrollDirection: Axis.vertical,
+                        children: <Widget>[
+                          store(
+                            img: 'assets/images/41.jpg',
+                            nameStore: 'Fire tiger',
+                          ),
+                          store(
+                            img: 'assets/images/42.jpg',
+                            nameStore: 'ชานม',
+                          ),
+                          store(
+                            img: 'assets/images/43.jpg',
+                            nameStore: 'ซูชิ',
+                          ),
+                          store(
+                            img: 'assets/images/44.jpg',
+                            nameStore: 'Odtomato',
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 100.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-Widget appBars({h, c}) {
-  return Container(
-    height: h,
-    color: Colors.redAccent,
-    child: Padding(
-      padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: RaisedButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(6.0),
-              ),
-              color: Colors.grey[200],
-              onPressed: () {
-                print('tap to search page');
-                Navigator.push(
-                    c, MaterialPageRoute(builder: (c) => SearchPage()));
-              },
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.search,
-                    color: Colors.black54,
-                  ),
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  Text(
-                    'ค้นหา',
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontFamily: mali,
-                      fontSize: 12.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(width: 20),
-          IconButton(
-            icon: Icon(Icons.account_circle),
-            iconSize: 60.0,
-            color: Colors.white,
-            onPressed: () {
-              Navigator.of(c).push(_createRoute(screen: ProfileUser()));
-            },
-          ),
-        ],
-      ),
-    ),
-  );
-}
+
 
 Widget store({
   String img,
